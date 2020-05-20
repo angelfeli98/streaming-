ff31ba4039d55968cc580081a52cc8d8392c9f51
angelfeli98 <36333913+angelfeli98@users.noreply.github.com>
2020-05-20 06:38:45 -0500

    Update app.js

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var api5 = require('./routers/playlist');
 
 app.use(bodyParser.urlencoded({extended:false}))
 app.use(bodyParser.json())
-app.use(cors({origin: 'http://localhost:4200'}))
+app.use(cors())
 
 app.use(function(req,res,next){
     //puede ser consumidad desde cualquier lugar
